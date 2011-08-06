be9e43e61b1b80fd7180a3543315f29469075ac3
Kevin Jude Deloria <akiwarheit@gmail.com>
2011-08-06 12:44:22 +0800

    Added static controller and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bbs::Application.routes.draw do
+  get "static/index"
+
   resources :events
   match "/events_today" => "events#show_today"
 
@@ -51,7 +53,7 @@ Bbs::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "static#index"
 
   # See how all your routes lay out with "rake routes"
 
