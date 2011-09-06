5a16e30590e20cef03aa0706c407ed21d1f74b1c
Kevin Jude Deloria <akiwarheit@gmail.com>
2011-09-06 08:47:58 +0800

    Retrieve all announcements

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -5,12 +5,14 @@ class StaticController < ApplicationController
     # @events = Event.all(:order => "when DESC")
     # @events = Event.this_week
     @events = Event.where(
-      :when => Date.today.beginning_of_week..Date.today.end_of_week).order(
-      "updated_at DESC")
+      :when => Date.today.beginning_of_week..Date.today.end_of_week)
     
     # @announcements = Announcement.where(
       # :created_at => Date.today.beginning_of_week..Date.today.end_of_week)
     
+    # @announcements = Announcement.where(
+      # "created_at" => Date.today.beginning_of_week..Date.today.end_of_week)
+      
     @announcements = Announcement.all
   end
 end
